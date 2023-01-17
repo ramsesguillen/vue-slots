87694a2c3048264d6df783044b7fecd3e6fb5b8d
guillen.salinas <guillen.salinas@infotec.mx>
2023-01-17 00:32:13 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="q-app">
+    <!-- <router-view /> -->
+    <h5 class="text-center">Slots</h5>
+    <hr>
+    <base-form ref="myForm" @on:reset="reset">
+      <template #main>
+        <q-input
+          class="col-xs-12 col-sm-3"
+          :rules="[ val => val && val.length > 0 || 'Campo requerido']"
+          v-model="name" square outlined reverse-fill-mask
+          label="First label"
+        />
+      </template>
+      <template #footer>
+        <q-btn label="Submit" type="submit" color="primary"/>
+        <!-- <q-btn label="click" @click="clicked" type="click" color="secondary"/> -->
+      </template>
+    </base-form>
+  </div>
+</template>
+
+<script>
+import BaseForm from "./components/BaseForm.vue"
+
+export default {
+  components: { BaseForm },
+  data () {
+    return {
+      name: null,
+      age: null,
+
+      accept: false
+    }
+  },
+  methods: {
+    clicked () {
+      this.$refs.myForm.slotClick();
+    },
+    reset () {
+      this.name = null;
+    }
+  },
+}
+</script>
---
+++ src/components/BaseForm.vue
@@ -0,0 +1,53 @@
+<template>
+<q-card style="width: 100%" class="q-mt-md">
+    <q-card-section>
+      <q-form
+        @submit="onSubmit"
+        @reset="onReset"
+        class="q-gutter-lg"
+        ref="slotForm"
+      >
+        <div class="row q-col-gutter-sm">
+          <slot name="main"></slot>
+        </div>
+        <q-card-actions align="right">
+          <slot name="footer"></slot>
+          <q-btn label="Reset" type="reset"  color="primary" flat class="q-ml-sm" />
+
+          <!-- <slot name="footer"></slot> -->
+        </q-card-actions>
+      </q-form>
+    </q-card-section>
+  </q-card>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      name: null,
+      age: null,
+
+      accept: false
+    }
+  },
+  // emits: ['on:reset'],
+
+  methods: {
+    onSubmit () {
+      console.log('SAVING...');
+    },
+
+    slotClick () {
+      // console.log('clicked')
+    },
+    onReset () {
+      this.$emit('on:reset');
+      // this.$refs.slotForm.reset();
+    },
+  },
+  mounted() {
+    console.log(this.$slots)
+  },
+}
+</script>
